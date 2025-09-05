386b664b4ae0f041c8f0c1948c0859470f676990
Spidy_Sain <sk.sudad.hossain@g.bracu.ac.bd>
2025-09-05 19:31:15 +0600

    ufo_beam.py

--- ufo_beam.py
+++ ufo_beam.py
@@ -1,4 +1,3 @@
-
 # ufo_beam.py
 # Step 2: landing + beam
 
@@ -9,21 +8,19 @@ from OpenGL.GLUT import GLUT_BITMAP_HELVETICA_18
 import math, time
 import ufo_base
 
-# WIN_W, WIN_H = 1000, 800
-# ufo_pos = [0.0, 0.0, 60.0]
-# ufo_yaw = 0.0
-# ufo_speed = 140.0
-# ufo_turn_speed = 100.0
-# hover_amp = 6.0
-# hover_speed = 2.0
 last_time = None
 keys_down = set()
 
+# ---------------- Beam Config ----------------
 beam_active = False
-beam_cooldown = 3.0
+beam_cooldown = 7.0
 beam_cooldown_left = 0.0
+
+beam_duration = 10.0   # <-- Beam stays active for 10 seconds
+beam_timer = 0.0       # countdown timer for active time
 beam_angle_deg = 18.0
 
+# ---------------- UFO States ----------------
 ufo_state = "flying"
 altitude_fly = 160
 altitude_land = 18.0
@@ -32,7 +29,8 @@ altitude_land = 18.0
 def draw_beam():
     pos = ufo_base.ufo_pos
     yaw = ufo_base.ufo_yaw
-    if not beam_active: return
+    if not beam_active:
+        return
     glPushMatrix()
     glDisable(GL_LIGHTING)
     glEnable(GL_BLEND)
@@ -40,28 +38,32 @@ def draw_beam():
     glTranslatef(pos[0], pos[1], 0.0)
     h = max(1.0, pos[2])
     top_r = 6.0
-    bottom_r = math.tan(math.radians(beam_angle_deg))*h + top_r
+    bottom_r = math.tan(math.radians(beam_angle_deg)) * h + top_r
     glColor4f(1.0, 0.95, 0.4, 0.45)
     quad = gluNewQuadric()
-    glTranslatef(0,0,0.1)
-    glRotatef(yaw, 0,0,1)
-    gluCylinder(quad, bottom_r, top_r, h-0.1, 24, 1)
+    glTranslatef(0, 0, 0.1)
+    glRotatef(yaw, 0, 0, 1)
+    gluCylinder(quad, bottom_r, top_r, h - 0.1, 24, 1)
     glDisable(GL_BLEND)
     glEnable(GL_LIGHTING)
     glPopMatrix()
 
 
 def try_toggle_beam():
-    global beam_active, beam_cooldown_left
-    if beam_active:
-        beam_active = False
-        beam_cooldown_left = beam_cooldown
-    else:
-        if beam_cooldown_left == 0.0:
-            beam_active = True
+    """Activate beam if ready. Beam runs for beam_duration, then cooldown."""
+    global beam_active, beam_cooldown_left, beam_timer
+    if not beam_active and beam_cooldown_left == 0.0:
+        beam_active = True
+        beam_timer = beam_duration   # start countdown
 
-# def reshape(w, h):
-#     global WIN_W, WIN_H, ASPECT
-#     WIN_W, WIN_H = max(1,w), max(1,h)
-#     ASPECT = WIN_W/float(WIN_H)
-#     glViewport(0,0,WIN_W,WIN_H)
+
+def update_beam(dt):
+    """Update beam timer and cooldown each frame."""
+    global beam_active, beam_cooldown_left, beam_timer
+    if beam_active:
+        beam_timer -= dt
+        if beam_timer <= 0:
+            beam_active = False
+            beam_cooldown_left = beam_cooldown
+    elif beam_cooldown_left > 0:
+        beam_cooldown_left = max(0.0, beam_cooldown_left - dt)
