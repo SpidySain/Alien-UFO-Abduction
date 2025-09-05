09aa8a823ee6f561c94c1011eb4dc128fc9c64fc
Spidy_Sain <sk.sudad.hossain@g.bracu.ac.bd>
2025-09-05 18:13:04 +0600

    ufo_beam.py

--- ufo_beam.py
+++ ufo_beam.py
@@ -28,16 +28,6 @@ ufo_state = "flying"
 altitude_fly = 160
 altitude_land = 18.0
 
-def draw_text_2d(x, y, s, font=GLUT_BITMAP_HELVETICA_18):
-    glColor3f(1,1,1)
-    glMatrixMode(GL_PROJECTION); glPushMatrix(); glLoadIdentity()
-    gluOrtho2D(0, ufo_base.WIN_W, 0, ufo_base.WIN_H)
-    glMatrixMode(GL_MODELVIEW); glPushMatrix(); glLoadIdentity()
-    glRasterPos2f(x, y)
-    for ch in s: glutBitmapCharacter(font, ord(ch))
-    glPopMatrix()
-    glMatrixMode(GL_PROJECTION); glPopMatrix()
-    glMatrixMode(GL_MODELVIEW)
 
 def draw_beam():
     pos = ufo_base.ufo_pos
